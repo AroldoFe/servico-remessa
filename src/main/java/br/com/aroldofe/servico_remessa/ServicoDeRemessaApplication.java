60c00e35ebe64cf1b558d5dc652baa958245a38f
Aroldo Felix <junioraroldo37@gmail.com>
2025-12-11 08:53:15 -0300

    Initial commit

---
+++ src/main/java/br/com/aroldofe/servico_remessa/ServicoDeRemessaApplication.java
@@ -0,0 +1,13 @@
+package br.com.aroldofe.servico_remessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeRemessaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicoDeRemessaApplication.class, args);
+    }
+
+}
